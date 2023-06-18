838591448304ccfdb3f77c80605d5f4ee3600b2e
pokitpeng <38129494+pokitpeng@users.noreply.github.com>
2023-06-18 11:20:07 +0800

    chore: update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- 添加不同语言
-    ensure_installed = { "vim", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+    ensure_installed = { "vim", "bash", "go", "javascript", "json", "toml", "lua", "python", "typescript", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
     ignore_install = { "help" },
   
     highlight = { enable = true },
